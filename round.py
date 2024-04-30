5a44aa3da54f4a71479cead5758b2cc78cabb4d9
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-04-30 15:25:39 -0300

    Aprendendo sobre o método len

--- round.py
+++ round.py
@@ -3,3 +3,14 @@ valor = 1.231432412354
 valor_arredondado = round(valor, 2)
 
 print(valor_arredondado)
+
+# Comando Len
+Lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Dicionario = {'nome': "Gabs", "idade": 22}
+Tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+String = "Gabs"
+
+print(len(Lista)) #10
+print(len(Dicionario)) #2
+print(len(Tupla)) #10
+print(len(String)) #4
